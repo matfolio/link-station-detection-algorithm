aac3c03f32b0b697a90c72ba826db25e5a728cc6
Muftau Abdulrahamon <tunde4mons@outlook.com>
2018-06-30 20:06:02 +0300

    reformating the content

--- src/data.js
+++ src/data.js
@@ -3,42 +3,42 @@
 
 const data = {
 	linkStations:[
-				{
-					x:0,
-					y:0,
-					r:10
-				},
-				{
-					x:20,
-					y:20,
-					r:5
-				},
-				{
-					x:10,
-					y:0,
-					r:12
-				},			
-			],
-			device_points:[
-				{
-					x:0,
-					y:0
-				},
-				{
-					x:100,
-					y:100
-				},
-				{
-					x:15,
-					y:10
-				},
-				{
-					x:18,
-					y:18
-				},
-			],
-			suitableLinkStation_power : [],
-			result: ""
+		{
+			x:0,
+			y:0,
+			r:10
+		},
+		{
+			x:20,
+			y:20,
+			r:5
+		},
+		{
+			x:10,
+			y:0,
+			r:12
+		},			
+	],
+	device_points:[
+		{
+			x:0,
+			y:0
+		},
+		{
+			x:100,
+			y:100
+		},
+		{
+			x:15,
+			y:10
+		},
+		{
+			x:18,
+			y:18
+		},
+	],
+	suitableLinkStation_power : [],
+	result: ""
 }
 
 export default data
